0750f4183f32f7b5f220b62132dbe20235dbfe82
Tomasz Kurcz <uint@users.noreply.github.com>
2022-12-27 20:54:48 +0100

    Brief documentation of `jtd-derive-macros`

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,3 +1,10 @@
+//! Proc macros for `jtd-derive`.
+//!
+//! This crate is tightly tied to the `jtd-derive` crate. It's **not meant to be
+//! used as a direct dependency** in your project. Instead, please depend on the
+//! [`jtd-derive`](https://docs.rs/jtd-derive) crate, which provides documentation
+//! and access to the derive macro.
+
 mod derive;
 
 use quote::ToTokens;
